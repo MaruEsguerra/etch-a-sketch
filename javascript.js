8687fcdb9b16e626d406b862fb65e31930707688
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-02-04 15:43:01 +0800

    feat: Adds drawing ability Allows user to draw on the grid via mouse hovers.

--- javascript.js
+++ javascript.js
@@ -2,13 +2,21 @@
 function createGrid() {
     for (let i = 0; i < 16 * 16; i++) {
         const gridContainer = document.querySelector("#container");
-
-        // Allows css flexbox to fix the grid
-        let square = document.createElement("div");
-        square.classList.add("square");
-
-        gridContainer.appendChild(square);
+        squareGrid = createSquare()
+        gridContainer.appendChild(squareGrid);
     }
+}
+
+// Creates the squares with hovering functionality
+function createSquare() {
+    // Allows css flexbox to fix the grid
+    let square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseenter", ()=>{
+        square.style.backgroundColor = "black";
+    });
+    return square;
 }
 
 createGrid()
