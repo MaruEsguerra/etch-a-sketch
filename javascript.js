1734dff074b70084056b640e13f003b77cabb2af
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-02-04 16:27:18 +0800

    feat: Adds grid resizing Allows user to change grid size within a limit (100).

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 // Actually creates the grid
-function createGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
         const gridContainer = document.querySelector("#container");
-        squareGrid = createSquare()
+        squareGrid = createSquare(size)
         gridContainer.appendChild(squareGrid);
     }
 }
 
 // Creates the squares with hovering functionality
-function createSquare() {
+function createSquare(size) {
     // Allows css flexbox to fix the grid
     let square = document.createElement("div");
     square.classList.add("square");
@@ -16,7 +16,36 @@ function createSquare() {
     square.addEventListener("mouseenter", ()=>{
         square.style.backgroundColor = "black";
     });
+
+    // Dynamically computes square size
+    const squareSize = 960 / size;
+
+    // Sets the square size
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+
     return square;
 }
 
-createGrid()
+// Handles making grid as per user specifications
+function resizeGrid() {
+    let size = prompt("Please input the new grid size.", 16);
+    if (size <= 100) {
+        const grid = document.querySelector("#container");
+        grid.innerHTML = "";
+        createGrid(size);
+    } else if (size > 100) {
+        alert("Please input a number no more than 100.");
+    }
+}
+
+const resizeBtn = document.querySelector("#resize");
+resizeBtn.addEventListener("click", resizeGrid);
+
+// Optional stuff to add:
+    // Color
+    // Opacity
+    // Saving
+    // Eraser
+
+createGrid(16)
